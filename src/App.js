44bf24801fcd12c2cab5f915ec96fce9b443317b
Haeun Grace Jeong <grace.he.jeong@gmail.com>
2021-12-15 12:01:45 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
 
 function App() {
 	return (
-		<div className='App'>
-			<h1>Hello Word - MERN</h1>
-		</div>
+		<Router>
+			<div className='App'>
+				<Navbar />
+				<div className='container'>
+					<Switch>
+						<Route exact path='/' />
+						<Route exact path='/about' component={About} />
+						<Route exact path='/list' />
+					</Switch>
+				</div>
+			</div>
+		</Router>
 	);
 }
 
