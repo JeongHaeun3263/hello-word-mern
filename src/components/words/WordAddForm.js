c26921fc3d7e52b4122c9c1205383e53e711199f
Haeun Grace Jeong <grace.he.jeong@gmail.com>
2022-01-07 22:08:46 -0500

    change the word form input to select

--- src/components/words/WordAddForm.js
+++ src/components/words/WordAddForm.js
@@ -1,4 +1,11 @@
-import { Button, Grid, Input, TextField } from '@mui/material';
+import {
+	Button,
+	Grid,
+	Input,
+	MenuItem,
+	Select,
+	TextField,
+} from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
 
@@ -21,18 +28,27 @@ const WordAddForm = ({ onAdd }) => {
 
 	return (
 		<Box component='form' ref={formRef}>
-			<input
-				ref={inputTitleRef}
-				type='text'
-				placeholder='Enter a form...'
-				required
-			/>
-			<input ref={inputFormRef} type='text' placeholder='Enter a form...' />
-			<input
-				ref={inputMeaningRef}
-				type='text'
-				placeholder='Enter a meaning...'
-			/>
+			<div className='word__add__form'>
+				<input
+					ref={inputTitleRef}
+					type='text'
+					placeholder='Enter a form...'
+					required
+				/>
+				<select ref={inputFormRef}>
+					<option value='form'>select the form</option>
+					<option value='none'>none</option>
+					<option value='verb'>verb</option>
+					<option value='adjective'>adjective</option>
+					<option value='none'>adverb</option>
+				</select>
+
+				<input
+					ref={inputMeaningRef}
+					type='text'
+					placeholder='Enter a meaning...'
+				/>
+			</div>
 			<Button onClick={onSubmit} variant='contained'>
 				Save
 			</Button>
